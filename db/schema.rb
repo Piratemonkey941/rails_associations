0ab5ea6cff24b1098bf8b38ed53dab020e4ed059
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-05 19:12:42 -0600

    rails has many through association

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_222919) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_06_010523) do
   create_table "accounts", force: :cascade do |t|
     t.string "account_number"
     t.integer "supplier_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["supplier_id"], name: "index_accounts_on_supplier_id"
+  end
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "physician_id", null: false
+    t.integer "patient_id", null: false
+    t.datetime "appointment_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["physician_id"], name: "index_appointments_on_physician_id"
   end
 
   create_table "authors", force: :cascade do |t|
@@ -33,6 +43,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_222919) do
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "physicians", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "suppliers", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -40,5 +62,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_222919) do
   end
 
   add_foreign_key "accounts", "suppliers"
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "appointments", "physicians"
   add_foreign_key "books", "authors"
 end
