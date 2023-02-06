4ab792b928a9eb5a837e5aa19e4742caa43b6d70
Piratemonkey941 <107444528+Piratemonkey941@users.noreply.github.com>
2023-02-05 19:52:59 -0600

    Self Join

--- db/migrate/20230206014137_create_employees.rb
+++ db/migrate/20230206014137_create_employees.rb
@@ -2,6 +2,7 @@ class CreateEmployees < ActiveRecord::Migration[7.0]
   def change
     create_table :employees do |t|
       t.string :name
+      t.reference :manager, foreign_key: {to_table: :employees}
 
       t.timestamps
     end
